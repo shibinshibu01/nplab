673f9d8d7f489765d1ca768b0006e5ecf49d5441
Shibin Shibu <shibinsb01@gmail.com>
2023-07-18 06:55:48 +0530

    child created

--- fork.c
+++ fork.c
@@ -2,35 +2,47 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
-int *bubbleSort(int *arr){
-    int i=0;
-    return *arr;
+int bubbleSort(int arr[],int num,pid_t pid){
+    
+    return 0;
 }
 
 int main(){
 
-    int num,*arr,*sorted;
+    int num,arr[100];
     pid_t pid;
 
     printf("Enter the no of elements to sort: ");
     scanf("%d",&num);
     printf("Enter the elements to sort: ");
     for(int i=0;i<num;i++){
-        scanf("%d",*arr);
-        arr++;
+        scanf("%d",&arr[i]);
     }
     printf("Unsorted Array: ");
     for(int i=0;i<num;i++){
-        printf("%d\t",*arr);
-        arr++;
+        printf("%d\t",arr[i]);
     }
 
     pid = fork();
     
     if(pid==0){
-        printf("Child Sort- Descending");
-        sorted=bubbleSort(arr);
+        printf("\nChild Sort- Descending\n");
+        bubbleSort(arr,num);
+        for(int i=num-1;i>=0;i--){
+            printf("%d\t",arr[i]);
+        }
+    } else if(pid>1){
+        printf("\nParent Sort- Ascending");
+        bubbleSort(arr,num);
+        for(int i=0;i<num;i++){
+            printf("%d\t",arr[i]);
+        }
+        wait(NULL);
+    } else{
+        printf("\nFork() failed!");
     }
     return 0;
 }
